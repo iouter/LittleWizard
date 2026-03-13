b8c4359f0d1b7e2fd1336fb81275583123b3a906
iOuter <3155186831@qq.com>
2026-03-13 15:07:32 +0800

    FreezingRay

---
+++ Cards/Common/FreezingRay.cs
@@ -0,0 +1,30 @@
+using BaseLib.Utils;
+using LittleWizard.Powers;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace LittleWizard.Cards.Common;
+
+public class FreezingRay() : LittleWizardCard(1, CardType.Attack, CardRarity.Common, TargetType.AllEnemies)
+{
+    private const string CalculatedWaterElement = "CalculatedWaterElement";
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new CalculationBaseVar(6),
+        new CalculationExtraVar(1),
+        new CalculatedVar(CalculatedWaterElement).WithMultiplier((card, target) => target?.GetPowerAmount<WaterElement>() ?? 0)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+        await CommonActions.CardAttack(this, cardPlay.Target, ((CalculatedVar) DynamicVars[CalculatedWaterElement]).Calculate(cardPlay.Target)).Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.CalculationExtra.UpgradeValueBy(1);
+    }
+}
