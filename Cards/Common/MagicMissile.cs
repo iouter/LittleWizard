f17ecca98e2810ebd840e42a1f0c1f85f2fdffd0
iOuter <3155186831@qq.com>
2026-03-14 15:45:54 +0800

    MagicMissile

---
+++ Cards/Common/MagicMissile.cs
@@ -0,0 +1,28 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace LittleWizard.Cards.Common;
+
+public class MagicMissile() : LittleWizardCard(0, CardType.Attack, CardRarity.Common, TargetType.RandomEnemy)
+{
+    protected override bool HasEnergyCostX => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(3, ValueProp.Unpowered)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CommonActions.CardAttack(this, cardPlay, hitCount: ResolveEnergyXValue()).Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(1);
+    }
+}
