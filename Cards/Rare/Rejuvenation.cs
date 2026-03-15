ebcc531cbe8ccdb8758b2aba55ee0d9e21da02bb
iOuter <3155186831@qq.com>
2026-03-15 18:09:59 +0800

    Rejuvenation

---
+++ Cards/Rare/Rejuvenation.cs
@@ -0,0 +1,30 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Cards;
+
+namespace LittleWizard.Cards.Rare;
+
+public class Rejuvenation() : LittleWizardCard(6, CardType.Power, CardRarity.Rare, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3)];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Ethereal];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var cards = await CommonActions.SelectCards(this, SelectionScreenPrompt, choiceContext,  PileType.Deck, count: DynamicVars.Cards.IntValue);
+        foreach (var card in cards)
+        {
+            await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+            await CardPileCmd.RemoveFromDeck(card);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
+}
