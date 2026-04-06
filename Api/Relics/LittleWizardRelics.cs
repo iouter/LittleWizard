0e1fb0ecb0efe2dbd5fec44ba22dba3227a32341
青白影 <376565306@qq.com>
2026-04-06 20:01:10 +0800

    遗物以及动画更新_4.6

--- Api/Relics/LittleWizardRelics.cs
+++ Api/Relics/LittleWizardRelics.cs
@@ -2,47 +2,41 @@ using System.Text;
 using BaseLib.Abstracts;
 using BaseLib.Extensions;
 using BaseLib.Utils;
-using LittleWizard.Api.Extensions;
 using LittleWizard.Character;
+using LittleWizard.Api.Extensions;
 
 namespace LittleWizard.Api.Relics;
 
 [Pool(typeof(LittleWizardRelicPool))]
 public abstract class LittleWizardRelics : CustomRelicModel
 {
-    protected override string BigIconPath => $"{GetBaseFileName()}.png".BigRelicImagePath();
-
-    public override string PackedIconPath => $"{GetBaseFileName()}.tres".TresRelicImagePath();
-
-    protected override string PackedIconOutlinePath => $"{GetBaseFileName()}_outline.tres".TresRelicImagePath();
-
-    private static string PascalToSnakeCase(string input)
-    {
+    private static string ToSnakeCase(string input){
         if (string.IsNullOrEmpty(input))
             return input;
 
+        var lower = input.ToLowerInvariant();
         var sb = new StringBuilder();
-        for (var i = 0; i < input.Length; i++)
-        {
-            var c = input[i];
-            if (char.IsUpper(c))
-            {
-                if (i > 0)
-                    sb.Append('_');
-                sb.Append(char.ToLowerInvariant(c));
-            }
-            else
-            {
+        bool lastWasSeparator = true;
+
+        foreach (char c in lower){
+            if (char.IsLetterOrDigit(c)){
                 sb.Append(c);
+                lastWasSeparator = false;
+            }else{
+                if (!lastWasSeparator){
+                    sb.Append('_');
+                    lastWasSeparator = true;
+                }
             }
         }
-
-        return sb.ToString();
+        return sb.ToString().Trim('_');
     }
 
-    private string GetBaseFileName()
-    {
+    private string GetBaseFileName(){
         var rawName = Id.Entry.RemovePrefix();
-        return PascalToSnakeCase(rawName);
+        return ToSnakeCase(rawName);
     }
+    protected override string BigIconPath => $"{GetBaseFileName()}.png".BigRelicImagePath();
+    public override string PackedIconPath => $"{GetBaseFileName()}.tres".TresRelicImagePath();
+    protected override string PackedIconOutlinePath => $"{GetBaseFileName()}_outline.tres".TresRelicImagePath();
 }
