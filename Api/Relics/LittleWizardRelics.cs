ffb173a08736ab3d30f7150d15ddd3acc0d370a6
青白影 <376565306@qq.com>
2026-04-06 14:44:19 +0800

    蛇形图标检索

--- Api/Relics/LittleWizardRelics.cs
+++ Api/Relics/LittleWizardRelics.cs
@@ -1,10 +1,39 @@
 using BaseLib.Abstracts;
+using BaseLib.Extensions;
 using BaseLib.Utils;
 using LittleWizard.Character;
+using LittleWizard.Api.Extensions;
+using System.Text;
 
 namespace LittleWizard.Api.Relics;
 
 [Pool(typeof(LittleWizardRelicPool))]
-public abstract class LittleWizardRelics : CustomRelicModel
-{
+public abstract class LittleWizardRelics : CustomRelicModel{
+    private static string PascalToSnakeCase(string input){
+        if (string.IsNullOrEmpty(input))
+            return input;
+
+        var sb = new StringBuilder();
+        for (int i = 0; i < input.Length; i++)
+        {
+            char c = input[i];
+            if (char.IsUpper(c)){
+                if (i > 0)
+                    sb.Append('_');
+                sb.Append(char.ToLowerInvariant(c));
+            }
+            else{sb.Append(c);
+            }
+        }
+        return sb.ToString();
+    }
+    private string GetBaseFileName(){
+        string rawName = Id.Entry.RemovePrefix();          
+        return PascalToSnakeCase(rawName);                 
+    }
+    protected override string BigIconPath => $"{GetBaseFileName()}.png".BigRelicImagePath();
+
+    public override string PackedIconPath => $"{GetBaseFileName()}.tres".TresRelicImagePath();
+
+    protected override string PackedIconOutlinePath => $"{GetBaseFileName()}_outline.tres".TresRelicImagePath();
 }
