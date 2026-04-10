abd56b04645419c88237bdcdfdda3aa9fc80d068
iOuter <3155186831@qq.com>
2026-04-10 22:03:14 +0800

    Update Retrain.cs

--- Cards/Rare/Retrain.cs
+++ Cards/Rare/Retrain.cs
@@ -6,7 +6,6 @@ using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Nodes.HoverTips;
 
 namespace LittleWizard.Cards.Rare;
 
