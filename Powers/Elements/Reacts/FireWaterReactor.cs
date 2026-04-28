3c479bc229e290d18f5b607c710344203c14e496
iOuter <3155186831@qq.com>
2026-04-28 21:58:53 +0800

    make reactors Internal Correlation

--- Powers/Elements/Reacts/FireWaterReactor.cs
+++ Powers/Elements/Reacts/FireWaterReactor.cs
@@ -1,9 +1,12 @@
 using LittleWizard.Api.Powers;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace LittleWizard.Powers.Elements.Reacts;
 
@@ -16,6 +19,46 @@ public class FireWaterReactor : LittleWizardPower
         "res://LittleWizard/images/powers/fire_and_water_element_reactor_power.png";
     public override string CustomBigIconPath =>
         "res://LittleWizard/images/powers/big/fire_and_water_element_reactor_power.png";
+
+    public override async Task BeforeApplied(
+        Creature target,
+        decimal amount,
+        Creature? applier,
+        CardModel? cardSource
+    )
+    {
+        await CreatureCmd.Damage(
+            new ThrowingPlayerChoiceContext(),
+            target,
+            amount,
+            ValueProp.Unpowered,
+            applier,
+            null
+        );
+        await PowerCmd.Apply<StrengthPower>(target, -amount, applier, null);
+    }
+
+    public override async Task AfterPowerAmountChanged(
+        PowerModel power,
+        decimal amount,
+        Creature? applier,
+        CardModel? cardSource
+    )
+    {
+        if (power != this || amount == Amount)
+        {
+            return;
+        }
+        await CreatureCmd.Damage(
+            new ThrowingPlayerChoiceContext(),
+            Owner,
+            amount,
+            ValueProp.Unpowered,
+            applier,
+            null
+        );
+        await PowerCmd.Apply<StrengthPower>(Owner, -amount, applier, null);
+    }
 
     public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
     {
