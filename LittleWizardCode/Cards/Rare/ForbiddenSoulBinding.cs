28a419f079294ed0e5ca19747e4403b3fed92e6b
iOuter <3155186831@qq.com>
2026-05-13 04:01:53 +0800

    using

--- LittleWizardCode/Cards/Rare/ForbiddenSoulBinding.cs
+++ LittleWizardCode/Cards/Rare/ForbiddenSoulBinding.cs
@@ -1,4 +1,3 @@
-using BaseLib.Utils;
 using LittleWizard.LittleWizardCode.Api.Animation;
 using LittleWizard.LittleWizardCode.Api.Cards;
 using MegaCrit.Sts2.Core.Commands;
--- LittleWizardCode/Cards/Uncommon/MagicTrajectory.cs
+++ LittleWizardCode/Cards/Uncommon/MagicTrajectory.cs
@@ -1,7 +1,6 @@
 using BaseLib.Utils;
 using LittleWizard.LittleWizardCode.Api.Cards;
 using LittleWizard.LittleWizardCode.Api.Powers;
-using LittleWizard.LittleWizardCode.Powers.Elements;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
