a5b73fe84d851a797e06e8842258944f8e183182
iOuter <3155186831@qq.com>
2026-05-22 03:59:49 +0800

    fix #103

--- LittleWizardCode/Cards/Rare/SelfHypnosis.cs
+++ LittleWizardCode/Cards/Rare/SelfHypnosis.cs
@@ -1,31 +1,22 @@
 using BaseLib.Utils;
-using LittleWizard.LittleWizardCode.Api;
 using LittleWizard.LittleWizardCode.Api.Animation;
 using LittleWizard.LittleWizardCode.Api.Cards;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace LittleWizard.LittleWizardCode.Cards.Rare;
 
 public class SelfHypnosis()
-    : LittleWizardCard(1, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy)
+    : LittleWizardCard(2, CardType.Skill, CardRarity.Rare, TargetType.AnyEnemy)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars =>
-        [
-            new DamageVar(17, ValueProp.Move),
-            new PowerVar<VulnerablePower>(3),
-            new PowerVar<WeakPower>(3),
-            new CardsVar(3),
-        ];
+        [new BlockVar(17, ValueProp.Move), new CardsVar(3)];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await CommonActions.CardAttack(this, cardPlay).Execute(choiceContext);
-        await Utils.GivePower<VulnerablePower>(this, cardPlay, choiceContext);
-        await Utils.GivePower<WeakPower>(this, cardPlay, choiceContext);
+        await CommonActions.CardBlock(this, cardPlay);
         await AnimationHelper.TriggerCastAnimationOwner(this);
 
         var cards = await CommonActions.SelectCards(
@@ -45,7 +36,7 @@ public class SelfHypnosis()
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Damage.UpgradeValueBy(2);
+        DynamicVars.Block.UpgradeValueBy(2);
         DynamicVars.Cards.UpgradeValueBy(1);
     }
 }
