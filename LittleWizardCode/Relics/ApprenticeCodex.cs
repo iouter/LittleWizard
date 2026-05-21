1e32b53e5cfb10c85297435cbabf9a47577e0398
iOuter <3155186831@qq.com>
2026-05-22 03:58:12 +0800

    fix #104

--- LittleWizardCode/Relics/ApprenticeCodex.cs
+++ LittleWizardCode/Relics/ApprenticeCodex.cs
@@ -1,11 +1,11 @@
+using LittleWizard.LittleWizardCode.Api.Extensions;
 using LittleWizard.LittleWizardCode.Api.Relics;
-using LittleWizard.LittleWizardCode.Character;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.Factories;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.Models;
 
 namespace LittleWizard.LittleWizardCode.Relics;
 
@@ -22,11 +22,19 @@ public class ApprenticeCodex : LittleWizardRelics
         if (side != Owner.Creature.Side || combatState.RoundNumber > 1)
             return;
         Flash();
-        var cards = ModelDb
-            .CardPool<LittleWizardCardPool>()
-            .GetUnlockedCards(Owner.UnlockState, Owner.RunState.CardMultiplayerConstraint)
-            .ToList();
-        var card = Owner.RunState.Rng.CombatCardGeneration.NextItem(cards);
+        var card = CardFactory
+            .GetDistinctForCombat(
+                Owner,
+                Owner
+                    .Character.CardPool.GetUnlockedCards(
+                        Owner.UnlockState,
+                        Owner.RunState.CardMultiplayerConstraint
+                    )
+                    .Where(c => c.Tags.Contains(CardTagExtensions.LittleWizardElement)),
+                1,
+                Owner.RunState.Rng.CombatCardGeneration
+            )
+            .FirstOrDefault();
         if (card == null)
         {
             return;
