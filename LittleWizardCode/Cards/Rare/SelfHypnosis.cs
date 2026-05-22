689577d436672cf95caa26a81d3cff79929a1b17
iOuter <3155186831@qq.com>
2026-05-22 19:36:11 +0800

    fix #123

--- LittleWizardCode/Cards/Rare/SelfHypnosis.cs
+++ LittleWizardCode/Cards/Rare/SelfHypnosis.cs
@@ -9,7 +9,7 @@ using MegaCrit.Sts2.Core.ValueProps;
 namespace LittleWizard.LittleWizardCode.Cards.Rare;
 
 public class SelfHypnosis()
-    : LittleWizardCard(2, CardType.Skill, CardRarity.Rare, TargetType.AnyEnemy)
+    : LittleWizardCard(2, CardType.Skill, CardRarity.Rare, TargetType.Self)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars =>
         [new BlockVar(17, ValueProp.Move), new CardsVar(3)];
