ba8383bb666e6ae75c30d1af05289fea317b2720
青白影 <376565306@qq.com>
2026-05-22 13:46:01 +0800

    Update Ignite.cs

--- LittleWizardCode/Cards/Uncommon/Ignite.cs
+++ LittleWizardCode/Cards/Uncommon/Ignite.cs
@@ -4,11 +4,12 @@ using LittleWizard.LittleWizardCode.Api.Cards;
 using LittleWizard.LittleWizardCode.Api.Extensions;
 using LittleWizard.LittleWizardCode.Powers.Cards;
 using LittleWizard.LittleWizardCode.Powers.Elements;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.HoverTips;
 
 namespace LittleWizard.LittleWizardCode.Cards.Uncommon;
 
@@ -16,15 +17,36 @@ public class Ignite()
     : LittleWizardCard(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
 {
     protected override HashSet<CardTag> CanonicalTags => [CardTagExtensions.LittleWizardElement];
-    protected override IEnumerable<IHoverTip> ExtraHoverTips =>[HoverTipFactory.FromPower<IgnitePower>()];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<IgnitePower>()];
     protected override IEnumerable<DynamicVar> CanonicalVars =>
-        [new DamageVar(5, ValueProp.Move), new PowerVar<IgnitePower>(1)];
+        [new DamageVar(5, ValueProp.Move), new PowerVar<IgnitePower>(1), new CardsVar(2)];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
         await CommonActions.CardAttack(this, play.Target).Execute(choiceContext);
-        if (play.Target!.GetPowerAmount<FireElement>() <= 0)
+        if (!play.Target!.HasPower<FireElement>())
+        {
             await Utils.GivePower<IgnitePower>(this, play, choiceContext);
+        }
+        else
+        {
+            for (var i = 0; i < DynamicVars.Cards.BaseValue; i++)
+            {
+                var cards = Owner
+                    .PlayerCombatState!.DrawPile.Cards.Where(c =>
+                        c.DynamicVars.ContainsKey(nameof(FireElement))
+                    )
+                    .ToList();
+                var card = Owner.RunState.Rng.CombatCardSelection.NextItem(cards);
+                if (card != null)
+                {
+                    await CardPileCmd.Add(card, PileType.Hand);
+                }
+            }
+        }
     }
 
     protected override void OnUpgrade()
