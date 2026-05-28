da08b9e2afa0a19a638274b6bd60572275cf60fb
青白影 <376565306@qq.com>
2026-05-28 13:19:28 +0800

    Update MagicMissile.cs
    修复魔法飞弹MagicMissile.cs不会减少振翅的层数的问题

--- LittleWizardCode/Cards/Rare/MagicMissile.cs
+++ LittleWizardCode/Cards/Rare/MagicMissile.cs
@@ -1,9 +1,11 @@
 using BaseLib.Utils;
 using LittleWizard.LittleWizardCode.Api.Animation;
 using LittleWizard.LittleWizardCode.Api.Cards;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace LittleWizard.LittleWizardCode.Cards.Rare;
@@ -24,6 +26,27 @@ public class MagicMissile()
             .CardAttack(this, cardPlay, hitCount: times)
             .Unpowered()
             .Execute(choiceContext);
+
+        var target = cardPlay.Target;
+        if (target == null && CombatState != null)
+        {
+            var enemies = CombatState.HittableEnemies;
+            if (enemies.Count > 0)
+                target = Owner.RunState.Rng.CombatTargets.NextItem(enemies);
+        }
+        if (target == null)
+            return;
+
+        var flutter = target.GetPower<FlutterPower>();
+        if (flutter != null)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                if (flutter.Amount <= 0)
+                    break;
+                await PowerCmd.Decrement(flutter);
+            }
+        }
 
         await AnimationHelper.TriggerCastAnimationOwner(this);
     }
