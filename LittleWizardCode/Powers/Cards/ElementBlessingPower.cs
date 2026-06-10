c8e7c9f9f4c9d3f1708c484a348d2fc511572a4b
青白影 <376565306@qq.com>
2026-06-10 13:47:23 +0800

    Update ElementBlessingPower.cs
    fix #254

--- LittleWizardCode/Powers/Cards/ElementBlessingPower.cs
+++ LittleWizardCode/Powers/Cards/ElementBlessingPower.cs
@@ -10,28 +10,38 @@ public class ElementBlessingPower : LittleWizardPower
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
-    public override int DisplayAmount => GetInternalData<Data>().CardPlayed;
     public override PowerInstanceType InstanceType => PowerInstanceType.Instanced;
+    public override int DisplayAmount => GetInternalData<Data>()?.CardPlayed ?? 0;
+
+    protected override object InitInternalData() => new Data();
 
     public override async Task AfterCardPlayed(PlayerChoiceContext context, CardPlay cardPlay)
     {
+        if (cardPlay?.Card == null)
+            return;
         var card = cardPlay.Card;
-        if (card.Owner != Owner.Player)
+
+        if (card.Owner?.Creature != Owner)
             return;
 
-        if (ElementHelper.IsElementCard(card))
+        if (!ElementHelper.IsElementCard(card))
+            return;
+
+        var data = GetInternalData<Data>();
+        if (data == null)
+            return;
+
+        if (data.CardPlayed > 0)
         {
-            if (GetInternalData<Data>().CardPlayed > 0)
-            {
+            if (card.Owner != null)
                 await PlayerCmd.GainEnergy(Amount, card.Owner);
-                GetInternalData<Data>().CardPlayed = 0;
-                InvokeDisplayAmountChanged();
-            }
-            else
-            {
-                GetInternalData<Data>().CardPlayed += 1;
-                InvokeDisplayAmountChanged();
-            }
+            data.CardPlayed = 0;
+            InvokeDisplayAmountChanged();
+        }
+        else
+        {
+            data.CardPlayed = 1;
+            InvokeDisplayAmountChanged();
         }
     }
 
