2216a059f3aeab72e1940b2d016368dfc709ef4e
青白影 <376565306@qq.com>
2026-06-12 10:16:03 +0800

    Update AnvilTransfer.cs

--- LittleWizardCode/Cards/Common/AnvilTransfer.cs
+++ LittleWizardCode/Cards/Common/AnvilTransfer.cs
@@ -6,7 +6,6 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Nodes.CommonUi;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace LittleWizard.LittleWizardCode.Cards.Common;
@@ -20,35 +19,26 @@ public class AnvilTransfer()
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
         await CommonActions.CardAttack(this, play).Execute(choiceContext);
-        if (Owner.Creature.Player == null || Owner.PlayerCombatState == null)
+        if (Owner.PlayerCombatState == null)
             return;
 
-        var handCards = Owner.PlayerCombatState.Hand.Cards;
-        var maxUpgrades = DynamicVars.Cards.IntValue;
-        if (maxUpgrades <= 0)
-            return;
-
-        var upgradableIndices = new List<int>();
-        for (int i = 0; i < handCards.Count; i++)
-        {
-            if (handCards[i].IsUpgradable)
-                upgradableIndices.Add(i);
-        }
-
+        var upgradeCount = DynamicVars.Cards.IntValue;
+        var upgradableCards = Owner
+            .PlayerCombatState.Hand.Cards.Where(c => c.IsUpgradable)
+            .ToList();
         var upgradedCards = new List<CardModel>();
-        var rng = Owner.RunState.Rng.CombatCardSelection;
-        for (int i = 0; i < maxUpgrades && upgradableIndices.Count > 0; i++)
+
+        for (int i = 0; i < upgradeCount && upgradableCards.Count > 0; i++)
         {
-            int randPos = rng.NextInt(upgradableIndices.Count);
-            int cardIndex = upgradableIndices[randPos];
-            var card = handCards[cardIndex];
+            var card = Owner.RunState.Rng.CombatCardSelection.NextItem(upgradableCards);
+            if (card == null)
+                continue;
             CardCmd.Upgrade(card);
             upgradedCards.Add(card);
-            upgradableIndices.RemoveAt(randPos);
+            upgradableCards.Remove(card);
         }
 
-        if (upgradedCards.Count > 0)
-            CardCmd.Preview(upgradedCards, time: 1.0f, CardPreviewStyle.HorizontalLayout);
+        CardCmd.Preview(upgradedCards, 1f);
 
         await AnimationHelper.TriggerCastAnimationOwner(this);
     }
