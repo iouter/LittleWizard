da38068077b4243c6a533f6111ec7ebba3305e89
青白影 <376565306@qq.com>
2026-06-21 13:27:08 +0800

    能量残余丢去垃圾堆
    fix #341

--- LittleWizardCode/Cards/Uncommon/ManagerMaster.cs
+++ LittleWizardCode/Cards/Uncommon/ManagerMaster.cs
@@ -1,14 +1,20 @@
+using BaseLib.Abstracts;
 using BaseLib.Utils;
+using HarmonyLib;
 using LittleWizard.LittleWizardCode.Api.Cards;
 using LittleWizard.LittleWizardCode.Powers.Cards;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Events;
 
 namespace LittleWizard.LittleWizardCode.Cards.Uncommon;
 
+[HarmonyPatch(typeof(TrashHeap), nameof(TrashHeap.Cards), MethodType.Getter)]
 public class ManagerMaster()
-    : LittleWizardCard(3, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+    : LittleWizardCard(3, CardType.Power, CardRarity.Event, TargetType.Self),
+        ITrashHeapCard
 {
     protected override IEnumerable<DynamicVar> CanonicalVars =>
         [new PowerVar<ManagerMasterPower>(1)];
