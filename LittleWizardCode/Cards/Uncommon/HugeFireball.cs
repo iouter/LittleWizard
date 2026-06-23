89f58efb47e2a5c1c97beee0eb52659e2fb79600
青白影 <376565306@qq.com>
2026-06-23 20:48:20 +0800

    实装火球术特效及其音效

--- LittleWizardCode/Cards/Uncommon/HugeFireball.cs
+++ LittleWizardCode/Cards/Uncommon/HugeFireball.cs
@@ -3,6 +3,8 @@ using LittleWizard.LittleWizardCode.Api;
 using LittleWizard.LittleWizardCode.Api.Cards;
 using LittleWizard.LittleWizardCode.Api.Extensions;
 using LittleWizard.LittleWizardCode.Powers.Elements;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
@@ -23,7 +25,12 @@ public class HugeFireball()
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await CommonActions.CardAttack(this, cardPlay).Execute(choiceContext);
+        AttackCommand attackCommand = await CommonActions
+            .CardAttack(this, cardPlay)
+            .WithHitFx("vfx/vfx_fire_ball")
+            .Execute(choiceContext);
+
+        SfxCmd.Play("res://LittleWizard/audios/cards/huge_fireball.wav");
         await CommonActions.Apply<FireElement>(choiceContext, this, cardPlay);
     }
 
