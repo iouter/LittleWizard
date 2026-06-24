586eeb306504185a9b061e4d7a1891ad5c5abe5d
iOuter <3155186831@qq.com>
2026-06-25 00:47:15 +0800

    fix #365

--- LittleWizardCode/Cards/Uncommon/Recall.cs
+++ LittleWizardCode/Cards/Uncommon/Recall.cs
@@ -3,9 +3,9 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 
-namespace LittleWizard.LittleWizardCode.Cards.Rare;
+namespace LittleWizard.LittleWizardCode.Cards.Uncommon;
 
-public class Recall() : LittleWizardCard(1, CardType.Skill, CardRarity.Rare, TargetType.Self)
+public class Recall() : LittleWizardCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
 {
     protected override Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
