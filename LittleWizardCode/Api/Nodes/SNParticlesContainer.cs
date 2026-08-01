cad55110c4e6d2ba49dd135e9624559cd8cbf3eb
iOuter <3155186831@qq.com>
2026-08-01 08:20:07 +0800

    use field

--- LittleWizardCode/Api/Nodes/SNParticlesContainer.cs
+++ LittleWizardCode/Api/Nodes/SNParticlesContainer.cs
@@ -1,18 +1,29 @@
+using System.Reflection;
 using Godot;
+using Godot.Collections;
 using MegaCrit.Sts2.Core.Nodes.Vfx.Utilities;
 
 namespace LittleWizard.LittleWizardCode.Api.Nodes;
 
 public partial class SNParticlesContainer : NParticlesContainer
 {
+    private static readonly FieldInfo? ParticlesField = typeof(NParticlesContainer).GetField(
+        "_particles",
+        BindingFlags.NonPublic | BindingFlags.Instance
+    );
+
     public override void _Ready()
     {
         base._Ready();
-        if (_particles != null && _particles.Count != 0)
+        var particles2Ds = GetParticles(this);
+        if (particles2Ds != null && particles2Ds.Count != 0)
             return;
-        _particles = [];
+        particles2Ds = [];
         foreach (var child in GetChildren())
             if (child is GpuParticles2D particles)
-                _particles.Add(particles);
+                particles2Ds.Add(particles);
     }
+
+    public static Array<GpuParticles2D>? GetParticles(NParticlesContainer instance) =>
+        ParticlesField?.GetValue(instance) as Array<GpuParticles2D>;
 }
