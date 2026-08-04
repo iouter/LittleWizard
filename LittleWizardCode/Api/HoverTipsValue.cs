242d0d50adfdb543188621bfe08efa39bd4c5718
mantuoluo911 <mantuoluo020911@gmail.com>
2026-08-04 21:15:19 +0800

    feat(ui): 为能量计数器添加交互式元素轮盘
    - 添加基础元素与元素反应的高亮贴图
    - 鼠标悬停时显示对应元素或反应的提示
    - 将元素轮盘集成到能量计数器场景中

--- LittleWizardCode/Api/HoverTipsValue.cs
+++ LittleWizardCode/Api/HoverTipsValue.cs
@@ -1,4 +1,5 @@
 using LittleWizard.LittleWizardCode.Powers.Elements;
+using LittleWizard.LittleWizardCode.Powers.Elements.Reacts;
 using MegaCrit.Sts2.Core.HoverTips;
 
 namespace LittleWizard.LittleWizardCode.Api;
@@ -8,4 +9,7 @@ public static class HoverTipsValue
     public static IHoverTip Fire => HoverTipFactory.FromPower<FireElement>();
     public static IHoverTip Water => HoverTipFactory.FromPower<WaterElement>();
     public static IHoverTip Earth => HoverTipFactory.FromPower<EarthElement>();
+    public static IHoverTip FireWater => HoverTipFactory.FromPower<FireWaterReactor>();
+    public static IHoverTip FireEarth => HoverTipFactory.FromPower<FireEarthReactor>();
+    public static IHoverTip WaterEarth => HoverTipFactory.FromPower<WaterEarthReactor>();
 }
