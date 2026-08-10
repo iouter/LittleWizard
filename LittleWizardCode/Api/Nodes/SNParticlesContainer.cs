b5ef19ff066d81879b96532f1e09a423b1d187ed
青白影 <142860251+qbyoffice@users.noreply.github.com>
2026-08-10 14:14:00 +0800

    SNParticlesContainer脚本修复
    进入场景树 _EnterTree()刷新获取的图片
    fix #355
    ps：加不加都无所谓（不影响正式版运行），毕竟我们没做beta兼容，这个也是缓兵之计，因为我们没做beta兼容，修复了这个照样还是会出现其他bug

--- LittleWizardCode/Api/Nodes/SNParticlesContainer.cs
+++ LittleWizardCode/Api/Nodes/SNParticlesContainer.cs
@@ -1,18 +1,32 @@
 using Godot;
+using Godot.Collections;
 using MegaCrit.Sts2.Core.Nodes.Vfx.Utilities;
 
 namespace LittleWizard.LittleWizardCode.Api.Nodes;
 
 public partial class SNParticlesContainer : NParticlesContainer
 {
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        EnsureInitialized();
+    }
+
     public override void _Ready()
     {
         base._Ready();
-        if (_particles != null && _particles.Count != 0)
+        EnsureInitialized();
+    }
+
+    private void EnsureInitialized()
+    {
+        if (_particles != null)
             return;
-        _particles = [];
+        _particles = new Array<GpuParticles2D>();
         foreach (var child in GetChildren())
-            if (child is GpuParticles2D particles)
-                _particles.Add(particles);
+        {
+            if (child is GpuParticles2D gp)
+                _particles.Add(gp);
+        }
     }
 }
